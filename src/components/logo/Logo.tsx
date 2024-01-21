89e26124b865f161709b889769fbdb31b3229d88
manhtu227 <manhtu2272002@gmail.com>
2024-01-21 14:29:26 +0700

    demo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -6,15 +6,15 @@ import { CircularXXWebBook } from '@assets/fonts/fonts';
 function Logo({ size, title }: { size?: string; title: string }) {
   console.log('LogoIcon', LogoIcon);
     return (
-        <div className='flex h-full items-center'>
-                         <LogoIcon
-                className={clsx({
-                    'h-[32px] w-[34px]': !size,
-                    'h-[10px] w-[10px]': size === 'sm',
-                    'mr-3': true,
-                })}
-            />
-            <div className={`text-2xl font-bold text-[#5BB9E2] ${CircularXXWebBook.className}`}>
+              <div className='flex h-full items-center'>
+                              <LogoIcon
+                      className={clsx({
+                          'h-[32px] w-[34px]': !size,
+                          'h-[10px] w-[10px]': size === 'sm',
+                          'mr-3': true,
+                      })}
+                  />
+                  <div className={`text-2xl font-bold text-[#5BB9E2] ${CircularXXWebBook.className}`}>
                 {title}
             </div>
         </div>
