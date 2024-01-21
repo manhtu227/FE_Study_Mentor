b501d487b9295da9a8cfff11d065e9d13a353764
manhtu227 <manhtu2272002@gmail.com>
2024-01-21 15:03:09 +0700

    fix

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -7,16 +7,16 @@ function Logo({ size, title }: { size?: string; title: string }) {
   console.log('LogoIcon', LogoIcon);
     return (
             <div className='flex h-full items-center'>
-                                                <LogoIcon
-                                        className={clsx({
-                                            'h-[32px] w-[34px]': !size,
-                                            'h-[10px] w-[10px]': size === 'sm',
-                                            'mr-3': true,
-                                        })}
-                                    />
-                                    <div className={`text-2xl font-bold text-[#5BB9E2] ${CircularXXWebBook.className}`}>
-            {title}
-        </div>  
+                                                                <LogoIcon
+                                                        className={clsx({
+                                                            'h-[32px] w-[34px]': !size,
+                                                            'h-[10px] w-[10px]': size === 'sm',
+                                                            'mr-3': true,
+                                                        })}
+                                                    />
+                                                    <div className={`text-2xl font-bold text-[#5BB9E2] ${CircularXXWebBook.className}`}>
+                            {title}
+                        </div>  
         </div>
     );
 }
