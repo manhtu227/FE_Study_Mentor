91aba2788ffc1e5510db8a154986fdc2f3bbc834
Vylee <87347391+Vylee29@users.noreply.github.com>
2024-03-20 19:24:32 +0700

    complete UI

---
+++ src/app/history-question/page.tsx
@@ -0,0 +1,9 @@
+import HistoryQuestionPage from '../../pages/history-question/HistoryQuestionPage';
+
+export default function FindMentor() {
+    return (
+        <main className='bg-[#F3F9FA]'>
+            <HistoryQuestionPage />
+        </main>
+    );
+}
---
+++ src/pages/history-question/HistoryQuestionPage.tsx
@@ -0,0 +1,50 @@
+'use client';
+import { RightOutlined } from '@ant-design/icons';
+import DocumentIcon from '@assets/icons/document-icon';
+import GlobeIcon from '@assets/icons/globe-icon';
+import WalletIcon from '@assets/icons/wallet-icon';
+import { CardTitleIcon } from '@components/card/CardTitleIcon';
+import { HistoryQuestion } from './components/HistoryQuestion';
+import { SideBarMentorProfile } from './components/SideBarMentorPorfile';
+
+export default function HistoryQuestionPage() {
+    return (
+        <div className='pack-layout pb-16 px-4'>
+            <div className='h-[69px] flex items-center gap-4 text-sm font-bold text-primary-800'>
+                <span>Người hướng dẫn</span>
+                <RightOutlined />
+                <span>Quản lý và thống kê doanh thu</span>
+            </div>
+            <div className='flex items-start w-full gap-8'>
+                <div className='w-[435px]'>
+                    <SideBarMentorProfile />
+                </div>
+                <div className='min-w-[500px] flex-grow'>
+                    <div className='mb-8'>
+                        <div className='flex gap-4 justify-between'>
+                            <CardTitleIcon
+                                title='Doanh thu'
+                                value='4.500 Xu'
+                                percent='+55%'
+                                icon={<WalletIcon />}
+                            />
+                            <CardTitleIcon
+                                title='Số câu trả lời'
+                                value='120'
+                                percent='+55%'
+                                icon={<DocumentIcon />}
+                            />
+                            <CardTitleIcon
+                                title='Thời gian truy cập'
+                                value='32 giờ'
+                                percent='+55%'
+                                icon={<GlobeIcon />}
+                            />
+                        </div>
+                    </div>
+                    <HistoryQuestion />
+                </div>
+            </div>
+        </div>
+    );
+}
