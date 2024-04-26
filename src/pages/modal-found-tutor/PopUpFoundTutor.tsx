9afec1e368c94d0d6fbdef580f812c19738439a2
Vylee <87347391+Vylee29@users.noreply.github.com>
2024-04-26 15:14:32 +0700

    [FEATURE] Found tutor (#51)

---
+++ src/pages/modal-found-tutor/PopUpFoundTutor.tsx
@@ -0,0 +1,8 @@
+import ModalFoundTutor from '@components/modal/ModalFoundTutor';
+export default function PopUpFoundTutor() {
+    return (
+        <div className='p-16'>
+            <ModalFoundTutor />
+        </div>
+    );
+}
