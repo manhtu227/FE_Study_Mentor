89ea783838b9dc07e7f78973e0ba52c5a908dd03
Vylee <87347391+Vylee29@users.noreply.github.com>
2024-04-26 12:16:20 +0700

    update UI

--- src/app/modal-accept-question/page.tsx
+++ src/app/modal-accept-question/page.tsx
@@ -1,6 +1,6 @@
 import PopUpAcceptQuestion from '../../pages/modal-accept-question/PopUpAcceptQuestion';
 
-export default function FindQuestion() {
+export default function AcceptQuestion() {
     return (
         <main className='flex flex-col items-center justify-between'>
             <PopUpAcceptQuestion />
--- src/components/modal/ModalAcceptQuestion.tsx
+++ src/components/modal/ModalAcceptQuestion.tsx
@@ -1,21 +1,38 @@
 'use client';
 import { QuestionCircleTwoTone } from '@ant-design/icons';
-import { Button, Flex } from 'antd';
+import { Modal } from 'antd';
+import { useState } from 'react';
 
 export default function ModalAcceptQuestion() {
+    const [isModalOpen, setIsModalOpen] = useState(true);
+
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
     return (
-        <div className='w-[900px] p-4 shadow-lg border flex flex-col text-center items-center'>
-            <h2 className='text-[20px] leading-[27px] text-[NeutralDark1]'>
-                Có câu hỏi mới cần được giải đáp
-            </h2>
-            <QuestionCircleTwoTone style={{ fontSize: '100px' }} className='p-5' />
-            <div>Bạn có muốn nhận trả lời câu hỏi?</div>
-            <div className='mt-5'>
-                <Flex gap='large' wrap='wrap'>
-                    <Button type='primary'>Chấp nhận</Button>
-                    <Button>Từ chối</Button>
-                </Flex>
-            </div>
+        <div>
+            <Modal
+                open={isModalOpen}
+                onOk={handleOk}
+                onCancel={handleCancel}
+                okText='Chấp nhận'
+                cancelText='Từ chối'
+                className='w-[500px] h-[500px] flex flex-col text-center items-center justify-center'
+            >
+                <h2 className='text-[20px] leading-[27px] text-[NeutralDark1]'>
+                    Có câu hỏi mới cần được giải đáp
+                </h2>
+                <QuestionCircleTwoTone style={{ fontSize: '100px' }} className='p-5' />
+                <div>Bạn có muốn nhận trả lời câu hỏi?</div>
+            </Modal>
         </div>
     );
 }
