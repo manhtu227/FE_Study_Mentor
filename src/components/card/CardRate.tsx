f90abdbe8b57e11181bf532e72a6e59c93e75999
Huynh Ngoc Son <69881676+huynhson0809@users.noreply.github.com>
2024-07-09 00:19:39 +0700

    fix bug profile page (#80)

--- src/components/card/CardRate.tsx
+++ src/components/card/CardRate.tsx
@@ -15,7 +15,7 @@ export const CardRate = () => {
                 </div>
                 <div className='text-sm font-normal text-black-800'>
                     Hãy tích cực và nhiệt tình trao đổi, hướng dẫn cho học sinh của bạn nhé. Nhận
-                    được đánh giá tốt là một cách tiếp cận và nân cao độ uy tín của bản thân nhé!
+                    được đánh giá tốt là một cách tiếp cận và nâng cao độ uy tín của bản thân nhé!
                 </div>
             </div>
             <ButtonOutlined title={'Xem nhận xét'} />
